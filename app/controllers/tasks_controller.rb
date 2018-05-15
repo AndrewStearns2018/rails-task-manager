a3d4fcc5fc3b52d109929e698882ef17cad0fdfb
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-15 12:00:07 +0200

    controller and router done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
